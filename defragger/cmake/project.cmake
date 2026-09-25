01b68fd999d6820c791d013bf3606c8fce1acd03
Shannon Smith <infiltratr@yandex.com>
2026-09-25 20:33:21 +1000

    Install approved raster UI artwork with Defragmenter

--- defragger/cmake/project.cmake
+++ defragger/cmake/project.cmake
@@ -538,6 +538,8 @@ install(FILES packaging/io.github.linuxdefragger.png
 install(FILES packaging/io.github.linuxdefragger.png
         DESTINATION lib/linux-defragger
         RENAME defragmenter-icon.png)
+install(FILES ../docs/ui/defragmenter-ui-vision.jpg
+        DESTINATION lib/linux-defragger/art)
 
 # Install only documentation that is part of the self-contained application
 # source tree. Canonical repository documentation lives at ../docs and is not
