8a0312c8d3a0cf52323e6aaab869cabcf366f6a7
Shannon Smith <infiltratr@yandex.com>
2026-09-26 20:28:03 +1000

    Report Stop readiness from xfs worker

--- defragger/gui/filesystems/xfs/native/xfs_worker.c
+++ defragger/gui/filesystems/xfs/native/xfs_worker.c
@@ -757,6 +757,7 @@ int main(int argc, char **argv) {
         fprintf(stderr, "%s: Growth Defrag requires exactly 10%%\n", PROGRAM_NAME); return 2;
     }
     ld_stop_clear(); ld_stop_install_handlers();
+    ld_stop_report_ready();
     char *error = NULL;
     int result;
     if (strcmp(operation, "recover") == 0) result = recover_transaction(device, journal, &error);
