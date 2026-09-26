36332a8ced897a5461801ca77588f345f698f85a
Shannon Smith <infiltratr@yandex.com>
2026-09-26 20:28:17 +1000

    Report Stop readiness from apfs worker

--- defragger/gui/filesystems/apfs/native/apfs_worker.c
+++ defragger/gui/filesystems/apfs/native/apfs_worker.c
@@ -782,6 +782,7 @@ int main(int argc, char **argv)
 
     ld_stop_clear();
     ld_stop_install_handlers();
+    ld_stop_report_ready();
     char error[512] = {0};
     if (recover) {
         const int rc =
