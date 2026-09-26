beb01c9e2f5ff0444bcc2bdbbd52f7a4dee1d00e
Shannon Smith <infiltratr@yandex.com>
2026-09-26 20:27:59 +1000

    Report Stop readiness from hfs worker

--- defragger/gui/filesystems/hfs/native/writer.c
+++ defragger/gui/filesystems/hfs/native/writer.c
@@ -1584,6 +1584,7 @@ int main(int argc, char **argv)
 
     ld_stop_clear();
     ld_stop_install_handlers();
+    ld_stop_report_ready();
     char *error = NULL;
     if (recover) {
         const int rc = recover_transaction(device, journal, live, &error);
